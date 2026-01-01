a8022bbf4395e765e17a88f88d03037b4563a076
Axenide <axenide@gmail.com>
2026-01-01 17:10:34 -0300

    fixed lockscreen

--- scripts/loginlock.sh
+++ scripts/loginlock.sh
@@ -3,19 +3,19 @@
 CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/Ambxst/config/system.json"
 
 get_lock_cmd() {
-    if [ -f "$CONFIG_FILE" ]; then
-        jq -r '.idle.general.lock_cmd // "ambxst lock"' "$CONFIG_FILE"
-    else
-        echo "ambxst lock"
-    fi
+	if [ -f "$CONFIG_FILE" ]; then
+		jq -r '.idle.general.lock_cmd // "ambxst lock"' "$CONFIG_FILE"
+	else
+		echo "ambxst lock"
+	fi
 }
 
 dbus-monitor --system "type='signal',interface='org.freedesktop.login1.Session',member='Lock'" |
-  while read -r line; do
-    if echo "$line" | grep -q "member=Lock"; then
-      COMMAND=$(get_lock_cmd)
-      if [ ! -z "$COMMAND" ]; then
-          eval "$COMMAND" &
-      fi
-    fi
-  done
+	while read -r line; do
+		if echo "$line" | grep -q "member=Lock"; then
+			COMMAND=$(get_lock_cmd)
+			if [ -n "$COMMAND" ]; then
+				eval "$COMMAND" &
+			fi
+		fi
+	done
